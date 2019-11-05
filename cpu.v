ed54300a851845d2373817a7282107f5a82d5e8f
melsafi1 <melsafi1@users.noreply.github.com>
2019-11-04 16:11:37 -0800

    changing state machine names
    used defines instead of binary

--- cpu.v
+++ cpu.v
@@ -1,6 +1,25 @@
 `define MREAD 2'b01
 `define MWRITE 2'b10
 
+`define RESET 5'b00000
+`define WImm 5'b00001
+`define LA 5'b00101
+`define LB 5'b00100
+`define ALU 5'b00110
+`define regW 5'b00111
+`define IF1 5'b01000
+`define readM2 5'b01001
+`define writeMD 5'b01010
+`define Mwrite 5'b01011
+`define readM 5'b01100
+`define SetADR 5'b01101
+`define LBR 5'b01110
+`define ALU2 5'b01111
+`define IF2 5'b10000
+`define updatePC 5'b11000
+`define decode 5'b11100
+`define HALT 5'b11111
+
 module cpu(clk,reset, mem_addr, mem_cmd, read_data, write_data);
 input clk, reset;
 input [15:0] read_data;
@@ -16,6 +35,9 @@ wire [11:0] FSM_out;
 wire [15:0] Ireg_out;
 wire [8:0] dataAddress;
 
+wire[2:0] Z_out;
+wire N,V,Z;
+
 wire load_pc, load_addr, load_ir, reset_pc, addr_sel;
 
 wire [8:0] next_pc,PC, intoOne,afterDataAddress;
@@ -28,12 +50,12 @@ Instruction_Dec ID1(.in(Ireg_out), .nsel(FSM_out[11:9]),	//inputs
 					.opcode(opcode), .op(op),				//to state machine
 					.shift(shift), .sximm8(sximm8), .sximm5(sximm5), .readOrWrite(readAndwrite));	//to Datapath
 
-FSM stateMachine(.opcode(opcode),.op(op),.reset(reset),.out(FSM_out),.clk(clk),.mem_cmd(mem_cmd), .addr_sel(addr_sel),.load_pc(load_pc),.reset_pc(reset_pc),.load_addr(load_addr), .load_ir(load_ir));
+FSM stateMachine(.opcode(opcode),.op(op),.reset(reset),.out(FSM_out),.clk(clk),.mem_cmd(mem_cmd), .addr_sel(addr_sel),.load_pc(load_pc),.reset_pc(reset_pc),.load_addr(load_addr), .load_ir(load_ir),.Z_out(Z_out),.N(N),.V(V),.Z(Z));
 
 
 datapath DP(.mdata(read_data), .sximm8(sximm8), .sximm5(sximm5), .PC(8'b0), .writenum(readAndwrite), .write(FSM_out[0]), 
 			.readnum(readAndwrite), .clk(clk), .asel(FSM_out[3]), .bsel(FSM_out[4]), .vsel(FSM_out[4:3]), .loadb(FSM_out[6]), .loada(FSM_out[5]), .loadc(FSM_out[7]), .loads(FSM_out[8]), .shift(shift), .ALUop(FSM_out[6:5]), 
-			.datapath_out(write_data), .Z_out(), .N(), .V(), .Z());
+			.datapath_out(write_data), .Z_out(Z_out), .N(N), .V(V), .Z(Z));
 			
 
 MUX #(9) PCMUX(.a(9'b0),.b(intoOne), .sel(reset_pc),.out(next_pc));
@@ -103,11 +125,16 @@ endmodule
 
 
 
-module FSM(opcode,op,reset,out,clk,mem_cmd, addr_sel,load_pc,reset_pc, load_addr, load_ir);
+
+
+module FSM(opcode,op,reset,out,clk,mem_cmd, addr_sel,load_pc,reset_pc, load_addr, load_ir,Z_out,N,V,Z);
 input clk;
 input[2:0] opcode;
 input[1:0] op;
 input reset;
+
+input [2:0] Z_out;
+input N,V,Z;
 
 output[11:0] out;
 output addr_sel,load_pc,reset_pc,load_addr, load_ir;
@@ -124,83 +151,83 @@ always@ (posedge clk) begin
 	casex ({state,reset,opcode,op})
 		
 		//reset
-		11'bxxxxx_1_xxx_xx : state = 0; //reset state is still 00000
+		11'bxxxxx_1_xxx_xx : state = `reset; //reset state is still 00000
 		
 		//reset stage
-		11'b00000_0_xxx_xx : state = 5'b01000;	//advance to IF1
+		{`reset,6'b0_xxx_xx} : state = `IF1;	//advance to IF1
 
 		//state 01000 IF1
-		11'b01000_0_xxx_xx : state = 5'b10000;	//advance to IF2
+		{`IF1,6'b0_xxx_xx} : state = `IF2;	//advance to IF2
 
 		//state 10000 IF2
-		11'b10000_0_xxx_xx : state = 5'b11000;	//advance to UpdatePC
+		{`IF2,6'b0_xxx_xx} : state = `updatePC;	//advance to UpdatePC
 
 		//state 11000 UpdatePC
-		11'b11000_0_xxx_xx : state = 5'b11100;	//advance to decode
+		{`updatePC,6'b0_xxx_xx} : state = `decode;	//advance to decode
 		
 		//state 11100 decode
-		11'b11100_0_110_10 : state = 5'b00001;	//MOV Rn,#<im8>
-		11'b11100_0_110_00 : state = 5'b00100;	//MOV Rd,Rm{,<sh_op>}
-		11'b11100_0_101_xx : state = 5'b00100;	//ALU instructions
-		11'b11100_0_011_00 : state = 5'b00101;	//LDR
-		11'b11100_0_100_00 : state = 5'b00101;	//STR
-		11'b11100_0_111_xx : state = 5'b11111;	//HALT
+		{`decode,6'b0_110_10} : state = `WImm;	//MOV Rn,#<im8>
+		{`decode,6'b0_110_00} : state = `LB;	//MOV Rd,Rm{,<sh_op>}
+		{`decode,6'b0_101_xx} : state = `LB;	//ALU instructions
+		{`decode,6'b0_011_00} : state = `LA;	//LDR
+		{`decode,6'b0_100_00} : state = `LA;	//STR
+		{`decode,6'b0_111_xx} : state = `HALT;	//HALT
 		//  ^^^ changed them to start taking messages from the decode state
 
 		//state 00100 Load B
-		11'b00100_0_101_0x : state = 5'b00101;	//Load A
-		11'b00100_0_101_10 : state = 5'b00101;	//Load A 
-		11'b00100_0_110_00 : state = 5'b00110;	//ALU
-		11'b00100_0_101_11 : state = 5'b00110;	//ALU
+		{`LB,6'b0_101_0x} : state = `LA;	//Load A
+		{`LB,6'b0_101_10} : state = `LA;	//Load A 
+		{`LB,6'b0_110_00} : state = `ALU;	//ALU
+		{`LB,6'b0_101_11} : state = `ALU;	//ALU
 
 		//state 00001 writeImm
-		11'b00001_0_xxx_xx : state = 5'b01000; 	//IF1
+		{`WImm,6'b0_xxx_xx} : state = `IF1; 	//IF1
 		
 		//state 00101 Load A
-		11'b00101_0_xxx_xx : state = 5'b00110;	//always goes to ALU
+		{`LA,6'b0_xxx_xx : state} = `ALU;	//always goes to ALU
 		
 		//state 00110 ALU operations
-		11'b00110_0_101_01 : state = 5'b01000;	//CMP - IF1
-		11'b00110_0_110_00 : state = 5'b00111;	//IF1
-		11'b00110_0_101_00 : state = 5'b00111;	//IF1
-		11'b00110_0_101_1x : state = 5'b00111;	//IF1
-		11'b00110_0_011_00 : state = 5'b01100;	//LDR - ReadMem
-		11'b00110_0_100_00 : state = 5'b01101;	//STR - SetARD
+		{`ALU,6'b0_101_01} : state = `IF1;	//CMP - IF1
+		{`ALU,6'b0_110_00} : state =`IF1;	//IF1
+		{`ALU,6'b0_101_00} : state = `IF1;	//IF1
+		{`ALU,6'b0_101_1x} : state = `IF1;	//IF1
+		{`ALU,6'b0_011_00} : state = `readM;	//LDR - ReadMem
+		{`ALU,6'b0_100_00} : state = `setADR;	//STR - SetARD
 		
 		//state 00111 writeReg
-		11'b00111_0_xxx_xx : state = 5'b01000;
+		{`regW,6'b0_xxx_xx} : state = `IF1;
 		
 		//state 01100 readMem (LDR)
-		11'b01100_0_xxx_xx : state = 5'b01001;	//always goes to writeMD
+		{`readM,6'b0_xxx_xx} : state = `readM2;	//always goes to writeMD
 		
 		//state 01001 readMem2 (LDR)
-		11'b01001_0_xxx_xx : state = 5'b01010;
+		{`readM2,6'b0_xxx_xx} : state = `writeMD;
 		
 		//state 01010 WriteMD (LDR)
-		11'b01010_0_xxx_xx : state = 5'b01000;	//always goes to IF1
+		{`writeMD,6'b00_xxx_xx} : state = `IF1;	//always goes to IF1
 		
 		//state 01101 SetADR (STR)
-		11'b01101_0_xxx_xx : state = 5'b01110;	//always goes to LoadBR
+		{`setADR,6'b0_xxx_xx} : state = `LBR;	//always goes to LoadBR
 		
 		//state 01110 LoadBR (STR)
-		11'b01110_0_xxx_xx : state = 5'b01111;	//always goes to ALU2
+		{`LBR,6'b0_xxx_xx} : state = `ALU2;	//always goes to ALU2
 		
 		//state 01111 ALU2 (STR)
-		11'b01111_0_xxx_xx : state = 5'b01011;	//always goes to Mwrite
+		{`ALU2,6'b0_xxx_xx} : state = `Mwrite;	//always goes to Mwrite
 		
 		//state 01011 Mwrite (STR)
-		11'b01011_0_xxx_xx : state = 5'b01000;	//always goes to IF1
+		{`Mwrite,6'b0_xxx_xx} : state = `IF1;	//always goes to IF1
 
 		//state 11111 HALT
-		11'b11111_0_xxx_xx : state = 5'b11111;
+		{`HALT,6'b0_xxx_xx} : state = `HALT;
 		
 		default : state = 5'bxxxxx;
 	endcase
 	
 	case (state)
 
-		//wait
-		5'b00000 : begin 
+		//reset
+		`reset : begin 
 		out = 12'b000_00000000_0;
 			//w = 2'b01;
 		reset_pc = 1;
@@ -208,7 +235,7 @@ always@ (posedge clk) begin
 		end
 		
 		//IF1
-		5'b01000 : begin
+		`IF1 : begin
 		out = 12'b000_00000000_0;
 		reset_pc = 0;
 		load_pc = 0;
@@ -217,80 +244,80 @@ always@ (posedge clk) begin
 		end
 		
 		//IF2
-		5'b10000 : begin
+		`IF2 : begin
 		addr_sel = 1;
 		mem_cmd = `MREAD;
 		load_ir = 1;
 		end
 
 		//Update PC
-		5'b11000 : begin
+		`updatePC : begin
 		mem_cmd = 2'b0;
 		load_ir = 0;
 		load_pc = 1;
 		end
 
 		//Decode
-		5'b11100 : begin
+		`decode : begin
 			load_pc = 0;
 			out = 12'b0000000_11_000;	//defaults selects to 1
 		end
 		
 		//writeImm
-		5'b00001 : begin
+		`WImm : begin
 			out = 12'b001_0000_10_00_1;
 		end
 		
 		//loadB
-		5'b00100 : begin
+		`LB : begin
 			out = {(7'b100_0010), 2'b01, (3'b0)};
 		end
 		
 		//loadA
-		5'b00101 : out = {(7'b001_0001),out[4], 1'b0, (3'b0)};
+		`LA : out = {(7'b001_0001),out[4], 1'b0, (3'b0)};
 		
 		//ALU instructions
-		5'b00110 : out = {3'b000_,({opcode,op}==5'b10101 ? 1 : 0),1'b1,op,out[4],out[3],3'b00_0};
+		`ALU : out = {3'b000_,({opcode,op}==5'b10101 ? 1 : 0),1'b1,op,out[4],out[3],3'b00_0};
 	
 		//writeReg
-		5'b00111 : out = 12'b010_00000000_1;
+		`regW : out = 12'b010_00000000_1;
 		
 		//readMem
 		5'b01100: begin
-		load_addr = 1; addr_sel = 0; mem_cmd = `MREAD;
+		`readM = 1; addr_sel = 0; mem_cmd = `MREAD;
 		end
 		
 		//readMem2
-		5'b01001 : begin
+		`readM2 : begin
 		end
 		
 		//writeMD
-		5'b01010 : begin
+		`writeMD : begin
 		out = 12'b010_000011001;
 		end
 		
 		//SetADR
-		5'b01101 : begin
+		`setADR : begin
 		load_addr = 1; addr_sel = 0;
 		end
 		
 		//LoadBR
-		5'b01110 : begin
+		`LBR : begin
 		out = {(7'b010_0010), 2'b01, (3'b0)};
 		end
 		
 		//ALU2
-		5'b01111 : begin
+		`ALU2 : begin
 		out = {5'b000_01,op,out[4],out[3],3'b00_0};
 		end
 		
 		//Mwrite
-		5'b01011 : begin
+		`Mwrite : begin
 		mem_cmd = `MWRITE;
 		end
 		
 		//HALT
-		5'b11111 : begin
+		`HALT : begin
 		end
 		
 		default : out = 12'b000000000000;
